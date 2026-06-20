8f45ebe6535f06a54698dddd5e5462e26fcfec70
Bui Duc Trung <buiductrung.cva@gmail.com>
2026-06-20 08:18:09 +0700

    Lesson 3

---
+++ Lesson3.py
@@ -0,0 +1,8 @@
+# ===================== Luyện tập =====================
+# Câu 1: Nhập một số từ bàn phím và in ra số đó.
+
+# Câu 2: Viết chương trình kiểm tra nhập vào 1 số và kiểm tra số đó là chẵn hay lẻ.
+
+# Câu 3: Viết chương trình tính tổng, hiệu, tích, thương, chia lấy nguyên, chia lấy dư, lũy thừa của hai số nhập từ bàn phím.
+
+# Câu 4: Viết chương trình chuyển đổi từ USD sang VND (số tiền được nhập từ bàn phím).
