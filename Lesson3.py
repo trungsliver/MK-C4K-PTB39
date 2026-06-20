e210da8d3af6577323a429a46d799454fe5c48ac
Bui Duc Trung <buiductrung.cva@gmail.com>
2026-06-20 08:35:31 +0700

    Lesson 3

--- Lesson3.py
+++ Lesson3.py
@@ -6,3 +6,10 @@
 # Câu 3: Viết chương trình tính tổng, hiệu, tích, thương, chia lấy nguyên, chia lấy dư, lũy thừa của hai số nhập từ bàn phím.
 
 # Câu 4: Viết chương trình chuyển đổi từ USD sang VND (số tiền được nhập từ bàn phím).
+
+# Bài 5: Nhập số điện bạn sử dụng (kWh)
+# Tính tiền điện theo dữ liệu sau và hiển thị ra màn hình
+# Bậc 1:    0kWh - 50kWh           giá 1.8k VND
+# Bậc 2:    51kWh - 100kWh         giá 2k VND
+# Bậc 3:    101kWh - 200kWh        giá 2.3k VND
+# Bậc 4:    trên 201kWh            giá 3k VND
