498f99b855fd76c14fbf1a022dfd88d46679a246
Bui Duc Trung <buiductrung.cva@gmail.com>
2026-07-04 09:58:09 +0700

    Lesson 5

---
+++ Lesson5.py
@@ -0,0 +1,29 @@
+# Quy tắc đặt tên file: [Lớp]_[Hoten]_CP1.py
+# Ví dụ: PTB39_DucTrung_CP1.py
+
+# Trắc nghiệm
+# 1A 2B 3C 4D ...
+
+# Tự luận
+# ............
+
+# Vấn đáp
+# Châu Anh: 2/3
+# Gia Linh: 3/3
+# Nam Phong: 0/3
+# Huy Quang: nghỉ
+# Khải Linh: 1.5/3
+# Đức Huy: 2/3
+# Bảo Phúc: 3/3
+
+# Kiểm tra lỗi
+    # Máy tính sẽ thử chạy câu lệnh trong "try"
+    # Nếu đúng: chạy bình thường, bỏ qua câu lệnh trong except
+    # Nếu sai: bỏ qua câu lệnh trong try, chạy câu lệnh trong except
+try:
+    num = 'hihi'
+    print("num // 2 =", num // 2)
+except:
+    print("Lỗi xảy ra khi thực hiện phép chia")
+
+print('Đây là lesson 5')
