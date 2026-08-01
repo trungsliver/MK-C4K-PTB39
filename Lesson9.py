53edc3f611a55085eb89f62ef4c17d9525d43fa4
Bui Duc Trung <buiductrung.cva@gmail.com>
2026-08-01 09:24:24 +0700

    Lesson 9

---
+++ Lesson9.py
@@ -0,0 +1,45 @@
+# Quy tắc đặt tên file: [Lớp]_[Hoten]_CP2.py
+# Ví dụ: PTB39_DucTrung_CP2.py
+
+# Link đề bài:: shorturl.at/azfDc
+
+# Trắc nghiệm
+# 1A 2B 3C 4D ...
+
+# Tự luận
+# ............
+
+# Các câu lệnh điều khiển vòng lặp
+    # break: thoát khỏi vòng lặp, bỏ qua các lần lặp còn lại
+    # countinue: bỏ qua lần lặp hiện tại, tiếp tục vòng lặp
+print('Ví dụ break:')
+for i in range(10):
+    if i == 5:
+        break
+    print(i, end = ' ')
+
+print('Ví dụ continue:')
+for i in range(10):
+    if i == 5:
+        continue
+    print(i, end = ' ')
+
+# Vấn đáp - Trắc nghiệm - Tự luận
+# Châu Anh:     2/2
+# Gia Linh:     2/2
+# Nam Phong:    
+# Huy Quang:    2/2
+# Khải Linh:    1.5/2
+# Đức Huy:      1/2
+# Bảo Phúc:     2/2
+
+# ========================= LUYỆN TẬP =========================
+# Bài 1: Viết chương trình nhập từ bàn phím danh sách a. Hãy trả về kết quả các phần tử trong danh sách theo thứ tự tăng dần.
+
+# Bài 2: Viết chương trình nhập từ bàn phím danh sách a. Hãy tìm ra phần tử lớn nhất và nhỏ nhất từ danh sách a và trả về kết quả.
+
+# Bài 3: Viết chương trình nhập từ bàn phím danh sách a. Hãy tính giá trị trung bình của các phần tử trong danh sách a và trả về kết quả giá trị trung bình.
+
+# Bài 4: Viết chương trình nhập từ bàn phím danh sách a. Tính tổng các số lẻ và tổng các số chẵn trong danh sách a.
+
+# Bài 5: Viết chương trình khai báo sẵn danh sách a. Viết chương trình bao gồm các chức năng: hiện toàn bộ phần tử danh sách, thêm phần tử vào danh sách, sửa phần tử danh sách, xóa phần tử trong danh sách.
