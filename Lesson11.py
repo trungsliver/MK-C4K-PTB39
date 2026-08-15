8c1387815051222903387e7b23e071df1f856604
Bui Duc Trung <buiductrung.cva@gmail.com>
2026-08-15 08:18:15 +0700

    Lesson 11

--- Lesson11.py
+++ Lesson11.py
@@ -34,3 +34,18 @@
     # YC5: Tìm giá trị phần tử lớn nhất của danh sách
     # YC6: Tìm vị trí phần tử nhỏ nhất của danh sách
     # YC7: Dùng hàm tính giá trị trung bình của các phần tử trong danh sách
+
+# Bài 10: Nhập vào từ bàn phím 1 chuỗi ký tự bao gồm các số nguyên, các số này cách nhau 1 khoảng trắng (hoặc dấu ,).
+    # Yêu cầu 1: Tách chuỗi và thêm vào danh sách có tên num_list
+    # Yêu cầu 2: Chuyển đổi toàn bộ phần tử trong danh sách num_list thành kiểu dữ liệu int.
+    # Yêu cầu 3: In ra màn hình số lượng số lẻ của num_list.
+
+# Bài 11: Dùng thư viên random để thêm ngẫu nhiên các số nguyên trong khoảng [20,50] vào 2 danh sách arr1 và arr2.
+    # Yêu cầu 1: Hãy viết hàm/chương trình con in ra phần tử chung của 2 danh sách vừa tạo
+    # Yêu cầu 2: In ra màn hình vị trí phần tử nhỏ nhất của danh sách arr1
+    # Yêu cầu 3: In ra màn hình vị trí phần tử lớn nhất của danh sách arr2
+
+# Bài 12: Hãy nhập từ bàn phím họ tên của bạn.
+    # Yêu cầu 1: Chuyển đổi chuỗi ký tự tên thành chuỗi ký tự viết hoa
+    # Yêu cầu 2: Chuyển đổi chuỗi ký tự tên thành chuỗi ký tự viết thường
+    # Yêu cầu 3: Chuyển đổi chuỗi ký tự tên thành chuỗi ký tự viết hoa các chữ cái đầu
